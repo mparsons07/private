facebook/react

commit 325322898cc413454feede0201b61da1abe1e67e

Throw when calling transferPropsTo() on a component you don't own
This is dangerous because it means that data is flowing into the component from two components, only one of which is the actual "owner". While we may be able to figure out how to
support this someday, let's be strict and prevent it for now.

src/core/ReactPropTransferer.js
@@ -19,6 +19,7 @@
 "use strict";
 
 var emptyFunction = require('emptyFunction');
+var invariant = require('invariant');
 var joinClasses = require('joinClasses');
 var merge = require('merge');
 
@@ -90,6 +91,13 @@ var ReactPropTransferer = {
      * @protected
      */
     transferPropsTo: function(component) {
+      invariant(
+        component.props.__owner__ === this,
+        'You can\'t call transferPropsTo() on a component that you don\'t ' +
+        'own. This usually means you are calling transferPropsTo() on a ' +
+        'component passed in as props or children.'
+      );
+
       var props = {};
       for (var thatKey in component.props) {
         if (component.props.hasOwnProperty(thatKey)) {

src/core/__tests__/ReactPropTransferer-test.js
@@ -25,7 +25,7 @@ var reactComponentExpect;
 
 var TestComponent;
 
-describe('ReactCompositeComponent-transferProps', function() {
+describe('ReactPropTransferer', function() {
 
   beforeEach(function() {
     React = require('React');
@@ -129,5 +129,28 @@ describe('ReactCompositeComponent-transferProps', function() {
     });
 
     ReactTestUtils.renderIntoDocument(<OuterOuterRefTestComponent />);
+  });
+
+  it('should not transferPropsTo() a component you don\'t own', function() {
+    var Parent = React.createClass({
+      render: function() {
+        return <Child><span /></Child>;
+      }
+    });
+
+    var Child = React.createClass({
+      render: function() {
+        return this.transferPropsTo(this.props.children);
+      }
+    });
+
+    expect(function() {
+      ReactTestUtils.renderIntoDocument(<Parent />);
+    }).toThrow(
+      'Invariant Violation: ' +
+      'You can\'t call transferPropsTo() on a component that you don\'t own. ' +
+      'This usually means you are calling transferPropsTo() on a component ' +
+      'passed in as props or children.'
+    );
   });
 });

mparsons07/private

commit 2bf4af86fac1bfab451c5ed45e41ad053407b905

Make transferPropsTo() message easier to debug
Summary:
Made the transferPropsTo() error introduced in
325322898cc413454feede0201b61da1abe1e67e easier to use to debug.

src/core/ReactPropTransferer.js
@@ -93,9 +93,11 @@ var ReactPropTransferer = {
     transferPropsTo: function(component) {
       invariant(
         component.props.__owner__ === this,
-        'You can\'t call transferPropsTo() on a component that you don\'t ' +
-        'own. This usually means you are calling transferPropsTo() on a ' +
-        'component passed in as props or children.'
+        '%s: You can\'t call transferPropsTo() on a component that you ' +
+        'don\'t own, %s. This usually means you are calling ' +
+        'transferPropsTo() on a component passed in as props or children.',
+        this.constructor.displayName,
+        component.constructor.displayName
       );
 
       var props = {};

src/core/__tests__/ReactPropTransferer-test.js
@@ -148,7 +148,8 @@ describe('ReactPropTransferer', function() {
       ReactTestUtils.renderIntoDocument(<Parent />);
     }).toThrow(
       'Invariant Violation: ' +
-      'You can\'t call transferPropsTo() on a component that you don\'t own. ' +
+      'Child: You can\'t call transferPropsTo() on a component that you ' +
+      'don\'t own, span. ' +
       'This usually means you are calling transferPropsTo() on a component ' +
       'passed in as props or children.'
     );